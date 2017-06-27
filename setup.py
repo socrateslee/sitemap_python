41e164b3a2c8b7f2fe9c91ed84d59c2fe0e7ea9b
lichun <lichun@gstianfu.com>
2017-06-27 12:25:48 +0800

    update setup.py for long_description; version 0.2.0

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
+import os
+del os.link
+
+long_description = ""
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    pass
 
 sdict = {
     'name': 'sitemap_python',
-    'version': "0.1.2",
+    'version': "0.2.0",
     'license': 'MIT',
     'packages': ['sitemap'],
     'zip_safe': False,
-    'install_requires': [],
+    'install_requires': ['six'],
     'author': 'Lichun',
+    'long_description': long_description,
     'url': 'https://github.com/socrateslee/sitemap_python',
     'classifiers': [
         'Environment :: Console',
